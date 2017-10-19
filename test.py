6d40d6b767b79108f2eca03c5353f90f06572fdf
eliothsu <eh7739@gmail.com>
2017-10-19 15:52:41 -0700

    Processing all data into 2-pairs

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import json
 from collections import Counter
+import operator
 
 def print_basic_info(filename):
     with open(filename) as data_file:
@@ -14,24 +15,45 @@ def print_basic_info(filename):
     print(c)
 
     games = [game for game in games if game["mode"] == "ranked"]
+    final_data = {}
     print("Number of ranked games: " + str(len(games)))
 
-    game = games[0]
-    card_history = game["card_history"]
-    card_history = [card['card'] for card in card_history if card['player'] == "opponent"]
-    card_history = [card for card in card_history if card['id'][-2:] != "t1" and card['id'] not in hero_powers]
-    # card['id'][:3] != "CS2" and 
-    print(card_history)
+    all_cards = parse_cards("cards.json")
+    hero_powers = parse_hero_powers("cards.json")
+    id_to_name = {}
+    for card in all_cards:
+        if 'id' in card and 'name' in card:
+            id_to_name[card['id']] = card['name']
 
+    for game in games:
+        card_history = game["card_history"]
+        card_history = [card['card'] for card in card_history if card['player'] == "opponent"]
+        card_history = [card['id'] for card in card_history if card['id'][-2:] != "t1" and card['id'] not in hero_powers]
+        # print(card_history)
+        for card_one in card_history:
+            for card_two in card_history:
+                if frozenset((card_one, card_two)) not in final_data:
+                    final_data[frozenset((card_one, card_two))] = 1
+                else:
+                    final_data[frozenset((card_one, card_two))] += 1
+
+    # print(final_data)
+    most_played_card_pair = max(final_data.keys(), key=(lambda key: final_data[key]))
+    for key in most_played_card_pair:
+        print(key)
     print()
 
 def parse_hero_powers(filename):
-    with open(filename, encoding="utf-8") as data_file:
-        data = json.load(data_file)
-    print(filename)
+    data = parse_cards("cards.json")
     return [card["id"] for card in data if "type" in card and card["type"] == "HERO_POWER"]
 
-hero_powers = parse_hero_powers("cards.json")
+def parse_cards(filename):
+    with open(filename, encoding="utf-8") as data_file:
+        data = json.load(data_file)
+    return data
+
+# all_cards = parse_cards("cards.json")
+# hero_powers = parse_hero_powers("cards.json")
 
 print_basic_info("2017-10-18.json")
 print_basic_info("2017-09.json")
