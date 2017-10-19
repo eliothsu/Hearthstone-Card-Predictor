8737b03a10e74c3a8224297f993f53a8c217519f
eliothsu <eh7739@gmail.com>
2017-10-19 12:14:39 -0700

    Working JSON data into a usable data structure

--- test.py
+++ test.py
@@ -6,11 +6,21 @@ def print_basic_info(filename):
         data = json.load(data_file)
     print(filename)
     print("Number of unique users: " + str(data["unique_users"]))
-    print("Number of games: " + str(data["total_games"]))
+    # print("Number of games: " + str(data["total_games"]))
     print("Number of games (hard count): " + str(len(data["games"])))
 
-    c = Counter(data["games"][x]["mode"] for x in range(len(data["games"])))
+    games = data["games"]
+    c = Counter(games[x]["mode"] for x in range(len(games)))
     print(c)
+
+    games = [game for game in games if game["mode"] == "ranked"]
+    print("Number of ranked games: " + str(len(games)))
+
+    game = games[0]
+    card_history = game["card_history"]
+    card_history = [card['card'] for card in card_history if card['player'] == "opponent"]
+    print(card_history)
+
     print()
 
 print_basic_info("2017-10-18.json")
