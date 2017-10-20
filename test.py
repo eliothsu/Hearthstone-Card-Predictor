737eadcbcab90dd116fa8fff78aa98e4a8a59cc7
Eliot Hsu <eliothsu@berkeley.edu>
2017-10-19 19:03:00 -0700

    Finished most of data manipulation

--- test.py
+++ test.py
@@ -16,6 +16,7 @@ def print_basic_info(filename):
 
     games = [game for game in games if game["mode"] == "ranked"]
     final_data = {}
+    paired_data = {}
     print("Number of ranked games: " + str(len(games)))
 
     all_cards = parse_cards("cards.json")
@@ -24,11 +25,12 @@ def print_basic_info(filename):
     for card in all_cards:
         if 'id' in card and 'name' in card:
             id_to_name[card['id']] = card['name']
+            id_to_name[card['name']] = card['id']
 
     for game in games:
         card_history = game["card_history"]
         card_history = [card['card'] for card in card_history if card['player'] == "opponent"]
-        card_history = [card['id'] for card in card_history if card['id'][-2:] != "t1" and card['id'] not in hero_powers]
+        card_history = [card['id'] for card in card_history if card['id'][-2:] != "t1" and card['id'] not in hero_powers and card['id'] != "GAME_005" and card['id'] != "GAME_005e"]
         # print(card_history)
         for card_one in card_history:
             for card_two in card_history:
@@ -37,11 +39,28 @@ def print_basic_info(filename):
                 else:
                     final_data[frozenset((card_one, card_two))] += 1
 
+                if card_one not in paired_data:
+                    paired_data[card_one] = {}
+                if card_two not in paired_data:
+                    paired_data[card_two] = {}
+                if card_one not in paired_data[card_two]:
+                    paired_data[card_two][card_one] = 0
+                if card_two not in paired_data[card_one]:
+                    paired_data[card_one][card_two] = 0
+                paired_data[card_one][card_two] += 1
+                paired_data[card_two][card_one] += 1
+
     # print(final_data)
     most_played_card_pair = max(final_data.keys(), key=(lambda key: final_data[key]))
+    top_cards = dict(sorted(final_data.items(), key=operator.itemgetter(1), reverse=True)[:5])
+    top_cards = [id_to_name[k] for pair in top_cards for k in pair]
+    print(top_cards)
     for key in most_played_card_pair:
-        print(key)
+        print(id_to_name[key])
+        # print([(id_to_name[k], final_data[frozenset((key, k))]) for k in paired_data[key]])
+    # print(id_to_name['The Coin'])
     print()
+    return final_data, paired_data
 
 def parse_hero_powers(filename):
     data = parse_cards("cards.json")
